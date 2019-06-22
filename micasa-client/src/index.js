42d55a34f6fcea2a5b751880cd069a5af3d303a0
github username <github email address>
2019-06-21 23:44:46 -0400

    updated cors to origin'*', added Provider wrapper'

--- micasa-client/src/index.js
+++ micasa-client/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import store from './store.js';
 
-console.log(store)
 ReactDOM.render(
-  <App store={store}/>,
+  <Provider store={store}>
+  <App />
+  </Provider>,
   document.getElementById('root')
 );
 
