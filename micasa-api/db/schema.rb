c47ff18e2d127e1d1830560b1c8afa1bcead15db
github username <github email address>
2019-07-01 13:32:36 -0400

    Added Review Model

--- micasa-api/db/schema.rb
+++ micasa-api/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_032255) do
+ActiveRecord::Schema.define(version: 2019_06_30_154542) do
 
   create_table "homes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.string "image"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "text"
+    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
